d8e28a64abd302b97d7f1af602feb5eb60e33f8c
cheeeze <hj951219@gmail.com>
2020-01-24 23:33:20 +0900

    글수정
    - 수정된 정보 전송
    - 수정된 내용 저장

--- main.js
+++ main.js
@@ -63,7 +63,7 @@ var app = http.createServer(function(request, response) {
       var description = "Hello, Node.js";
       var list = templateList(filelist);
       var template = templateHTML(title, list, `
-        <form action="http://localhost:3000/create_process" method="post">
+        <form action="/create_process" method="post">
           <p><input type="text" name="title" placeholder="title"></p>
           <p>
             <textarea name="description" placeholder="description"></textarea>
@@ -88,7 +88,45 @@ var app = http.createServer(function(request, response) {
         response.end();
       })
     });
-  } else {
+  } else if(pathname === '/update'){
+    fs.readdir('./data',function(error, filelist){
+      fs.readFile(`data/${queryData.id}`, "utf8", function(err, description) {
+        var title = queryData.id;
+        var list = templateList(filelist);
+        var template = templateHTML(title, list, `
+        <form action="update_process" method="post">
+          <input type="hidden" name="id" value="${title}">
+          <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+          <p>
+            <textarea name="description" placeholder="description">${description}</textarea>
+          </p>
+          <p><input type="submit"></p>
+        </form>
+      `, '');
+        response.writeHead(200);
+        response.end(template);
+      });
+    });
+  }else if(pathname === '/update_process'){
+    var body = '';
+    request.on('data', function(data){
+      body = body + data;
+    });
+    request.on('end',function(){
+      var post = qs.parse(body);
+      var id = post.id;
+      var title = post.title;
+      var description = post.description;
+      fs.rename(`data/${id}`, `data/${title}`,function(error){
+        fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+          response.writeHead(302, {Location: `/?id=${title}`});
+          response.end();
+        })
+      });
+      console.log(post);
+      
+    });
+  }else {
     response.writeHead(404);
     response.end("Not found");
   }
